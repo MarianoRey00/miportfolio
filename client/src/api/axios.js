377e38038d05b6491d6602de4b8ff3c219339ea2
mariano <reygonzalezmariano@gmail.com>
2025-02-24 13:16:02 -0300

    test

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-	baseURL: "https://miportfolio-rouge.vercel.app/api",
+	baseURL: "https://miportfolio-api.onrender.com/api",
 	withCredentials: true,
 });
 

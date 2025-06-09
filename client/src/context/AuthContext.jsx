cf5a9b4ebec862e1310325a18c82be6e9d5e4812
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-09 14:46:49 -0300

    Auth context

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -77,27 +77,55 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const checkLogin = async () => {
+      const cookies = Cookies.get();
+      if (!cookies.token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        return;
+      }
       try {
+        console.log("estoy vivo butito");
         const res = await verifyTokenRequest();
         if (!res.data) {
-          setIsAuthenticated(false);
-          setAuthUser(null);
-          return;
+          return setIsAuthenticated(false);
         }
         setIsAuthenticated(true);
         setAuthUser(res.data);
         if (res.data.role === "Admin") {
           setIsAdmin(true);
         }
+        setLoading(false);
       } catch (error) {
         setIsAuthenticated(false);
-        setAuthUser(null);
-      } finally {
         setLoading(false);
       }
     };
     checkLogin();
   }, []);
+
+  //   useEffect(() => {
+  //     const checkLogin = async () => {
+  //       try {
+  //         const res = await verifyTokenRequest();
+  //         if (!res.data) {
+  //           setIsAuthenticated(false);
+  //           setAuthUser(null);
+  //           return;
+  //         }
+  //         setIsAuthenticated(true);
+  //         setAuthUser(res.data);
+  //         if (res.data.role === "Admin") {
+  //           setIsAdmin(true);
+  //         }
+  //       } catch (error) {
+  //         setIsAuthenticated(false);
+  //         setAuthUser(null);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
+  //     checkLogin();
+  //   }, []);
 
   return (
     <AuthContext.Provider

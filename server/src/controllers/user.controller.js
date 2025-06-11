c5bdf232a70cc43f36cd02a0c89c442516c11662
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-11 18:33:16 -0300

    picture

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -58,7 +58,7 @@ export const login = async (req, res) => {
     username: user.username,
     email: user.email,
     role: user.role,
-    picture: user.picture?.url,
+    picture: user.picture,
   });
 };
 
@@ -85,7 +85,7 @@ export const verifyToken = async (req, res) => {
       username: userFound.username,
       email: userFound.email,
       role: userFound.role,
-      picture: userFound.picture?.url,
+      picture: userFound.picture,
     });
   });
 };

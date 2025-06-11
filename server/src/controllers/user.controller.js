05cae033971e095e974da694ddd72cc58b5dbddd
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-11 10:57:22 -0300

    register cookie

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -35,7 +35,10 @@ export const register = async (req, res) => {
     await createAppearance(newUser._id);
 
     const token = await createAccessToken({ id: newUser._id });
-    res.cookie("token", token);
+    res.cookie("token", token, {
+      secure: true,
+      sameSite: "None",
+    });
     res.json(newUser);
   } catch (error) {
     res.status(500).json({ message: error.message });

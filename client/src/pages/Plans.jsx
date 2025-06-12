6984a08d6e9f6d2777e07b05b69329e44b408811
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-12 08:21:25 -0300

    plans

--- client/src/pages/Plans.jsx
+++ client/src/pages/Plans.jsx
@@ -7,6 +7,64 @@ function Plans() {
         <div className="flex justify-center">
           <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden w-[50%]">
             <div class="p-8 border-b border-gray-200 bg-customColor-blue">
+              <h2 class="text-3xl font-semibold text-white">Plan Gratuito</h2>
+              <p className="text-lg">
+                Ideal para quienes quieren empezar a mostrar sus proyectos
+              </p>
+            </div>
+
+            <div class="p-8">
+              <div class="flex items-start">
+                <div class="ml-2 flex-1">
+                  <h3 class="text-2xl font-semibold text-gray-900">$0</h3>
+                  <p class="text-gray-600 mt-1 text-xl">Duracion ilimitada</p>
+                </div>
+              </div>
+
+              <div class="mt-6 pt-6 border-t border-gray-200">
+                <h4 class="text-sm font-medium text-gray-900 mb-3">Incluye:</h4>
+                <ul class="space-y-4">
+                  <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5 font-bold">
+                      &#10003;
+                    </span>
+                    <span class="text-gray-600 text-lg">
+                      Hasta 10 proyectos
+                    </span>
+                  </li>
+                  <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span class="text-gray-600 text-lg">
+                      Hasta 6 fotos por proyecto
+                    </span>
+                  </li>
+                  <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span class="text-gray-600 text-lg">
+                      Modificar la apariencia del perfil
+                    </span>
+                  </li>
+                  {/* <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span class="text-gray-600 text-lg">Subir PDFs</span>
+                  </li>
+                  <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span class="text-gray-600 text-lg">Compartir tu CV</span>
+                  </li>
+                  <li class="flex items-start">
+                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span class="text-gray-600 text-lg">
+                      Eliminar boton de miportfolio en el perfil
+                    </span>
+                  </li> */}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden w-[50%]">
+            <div class="p-8 border-b border-gray-200 bg-customColor-blue">
               <h2 class="text-3xl font-semibold text-white">
                 Plan profesional
               </h2>
@@ -27,7 +85,9 @@ function Plans() {
                 <h4 class="text-sm font-medium text-gray-900 mb-3">Incluye:</h4>
                 <ul class="space-y-4">
                   <li class="flex items-start">
-                    <span className="text-green-600 mr-2 mt-0.5">&#10003;</span>
+                    <span className="text-green-600 mr-2 mt-0.5 font-bold">
+                      &#10003;
+                    </span>
                     <span class="text-gray-600 text-lg">
                       Proyectos ilimitados
                     </span>

f2741f9c1d9872231f79a79b5d17b1dfe22e5ab9
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-12 07:32:53 -0300

    plan professional style

--- client/src/pages/Plans.jsx
+++ client/src/pages/Plans.jsx
@@ -4,65 +4,23 @@ function Plans() {
   return (
     <>
       <div className="min-h-screen">
-        <div className="flex">
-          <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden">
-            <div class="px-6 py-4 border-b border-gray-200 bg-orange-50">
-              <h2 class="text-lg font-semibold text-gray-900">Plan Gratuito</h2>
-              {/* <p>El plan adecuado</p> */}
+        <div className="flex justify-center">
+          <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden w-[50%]">
+            <div class="p-6 border-b border-gray-200 bg-customColor-blue">
+              <h2 class="text-2xl font-semibold text-white">
+                Plan profesional
+              </h2>
             </div>
 
             <div class="p-6">
               <div class="flex items-start">
-                <div class="ml-4 flex-1">
-                  <h3 class="text-lg font-medium text-gray-900">$0</h3>
-                  <p class="text-gray-600 mt-1">Duracion ilimitada</p>
+                <div class="ml-2 flex-1">
+                  <h3 class="text-xl font-semibold text-gray-900">$1000</h3>
+                  <p class="text-gray-600 mt-1 text-lg">Duracion ilimitada</p>
                 </div>
               </div>
 
               <div class="mt-6 pt-6 border-t border-gray-200">
-                <h4 class="text-sm font-medium text-gray-900 mb-3">Incluye:</h4>
-                <ul class="space-y-2">
-                  <li class="flex items-start">
-                    <span className="text-green-400 mr-2 mt-0.5">&#10003;</span>
-                    <span class="text-gray-600">Up to 25 projects</span>
-                  </li>
-                  <li class="flex items-start">
-                    <span className="text-green-400 mr-2 mt-0.5">&#10003;</span>
-                    <span class="text-gray-600">100GB storage</span>
-                  </li>
-                  <li class="flex items-start">
-                    <span className="text-green-400 mr-2 mt-0.5">&#10003;</span>
-                    <span class="text-gray-600">Priority support</span>
-                  </li>
-                  <li class="flex items-start">
-                    <span className="text-green-400 mr-2 mt-0.5">&#10003;</span>
-                    <span class="text-gray-600">
-                      Team collaboration (up to 10 members)
-                    </span>
-                  </li>
-                  <li class="flex items-start">
-                    <span className="text-green-400 mr-2 mt-0.5">&#10003;</span>
-                    <span class="text-gray-600">Advanced analytics</span>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div class="bg-white rounded-xl shadow-sm border border-gray-200 overflow-hidden">
-            <div class="px-6 py-4 border-b border-gray-200 bg-customColor-blue">
-              <h2 class="text-lg font-semibold text-white">Plan profesional</h2>
-            </div>
-
-            <div class="p-6">
-              <div class="flex items-start">
-                <div class="ml-4 flex-1">
-                  <h3 class="text-lg font-medium text-gray-900">$1000</h3>
-                  <p class="text-gray-600 mt-1">Duracion ilimitada</p>
-                </div>
-              </div>
-
-              <div class="mt-3 pt-6 border-t border-gray-200">
                 <h4 class="text-sm font-medium text-gray-900 mb-3">Incluye:</h4>
                 <ul class="space-y-2">
                   <li class="flex items-start">

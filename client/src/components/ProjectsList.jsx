072a56ba0a25a533370ee9c02062745b244f4054
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-13 20:51:13 -0300

    Appearance

--- client/src/components/ProjectsList.jsx
+++ client/src/components/ProjectsList.jsx
@@ -1,37 +1,45 @@
 export function ProjectList({ appearance, projects }) {
-	const publicProjects = projects.filter((project) => project.public);
-	return (
-		<>
-			{publicProjects.map((project) => (
-				<div
-					className="flex p-2 overflow-hidden mb-2"
-					style={{
-						backgroundColor: appearance.projectBackgroundColor,
-						borderRadius: appearance.projectShape,
-						border: appearance.projectBorder,
-						borderColor: appearance.projectBorderColor,
-					}}
-				>
-					<div className="w-[30%]">
-						<img
-							className="w-12 h-12 object-cover"
-							style={{
-								borderRadius: appearance.projectPictureShape,
-							}}
-							src={project.image?.url}
-							alt=""
-						/>
-					</div>
-					<div className="flex items-center w-[70%]">
-						<h2
-							className="text-[11px] break-words line-clamp-3 w-full"
-							style={{ color: appearance.projectTextColor }}
-						>
-							{project.title}
-						</h2>
-					</div>
-				</div>
-			))}
-		</>
-	);
+  const publicProjects = projects.filter((project) => project.public);
+  const modifiedProjectShape =
+    appearance.projectShape === "18px" ? "12px" : appearance.projectShape;
+
+  const modifiedProjectPictureShape =
+    appearance.projectPictureShape === "12px" ? "8px" : appearance.projectShape;
+
+  console.log(modifiedProjectPictureShape);
+
+  return (
+    <>
+      {publicProjects.map((project) => (
+        <div
+          className="flex p-2 overflow-hidden mb-2"
+          style={{
+            backgroundColor: appearance.projectBackgroundColor,
+            borderRadius: modifiedProjectShape,
+            border: appearance.projectBorder,
+            borderColor: appearance.projectBorderColor,
+          }}
+        >
+          <div className="w-[30%]">
+            <img
+              className="w-12 h-12 object-cover"
+              style={{
+                borderRadius: modifiedProjectPictureShape,
+              }}
+              src={project.image?.url}
+              alt=""
+            />
+          </div>
+          <div className="flex items-center w-[70%]">
+            <h2
+              className="text-[11px] break-words line-clamp-3 w-full"
+              style={{ color: appearance.projectTextColor }}
+            >
+              {project.title}
+            </h2>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }

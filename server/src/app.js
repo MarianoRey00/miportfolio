970bfea8f47912492f4db457493e669fc629c186
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-13 23:44:42 -0300

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -28,9 +28,9 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    // origin: "https://miportfolio18.vercel.app",
+    origin: "https://miportfolio18.vercel.app",
     // origin: "http://localhost:5173",
-    origin: "https://web.postman.co",
+    // origin: "https://web.postman.co",
     credentials: true,
   })
 );

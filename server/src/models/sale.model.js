7d3606df298031e15836355274537a762d82d355
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-13 22:43:48 -0300

    sale schema

--- server/src/models/sale.model.js
+++ server/src/models/sale.model.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const saleSchema = new mongoose.Schema({
+const purchaseSchema = new mongoose.Schema({
   title: String,
   price: Number,
   status: String,
   buyer: String,
   createdAt: { type: Date, default: Date.now },
 });
-export default mongoose.model("Sale", saleSchema);
+export default mongoose.model("Sale", purchaseSchema);

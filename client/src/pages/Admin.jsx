3a288b28613a411a7a35eba7963f5f4b4e74f104
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-15 08:37:17 -0300

    Admin sales

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -4,9 +4,9 @@ import Sidebar from "../components/Sidebar.jsx";
 function Admin() {
   return (
     <>
-      <div className="flex p-4">
+      <div className="flex p-4 min-h-screen">
         <Sidebar />
-        <div className="w-[80%] min-h-screen ml-[20%] bg-zinc-800 p-8 rounded-xl overflow-auto">
+        <div className="w-[80%]  ml-[20%] bg-zinc-800 p-8 rounded-xl overflow-auto scrollbar">
           <Outlet />
         </div>
       </div>
--- client/src/pages/Sales.jsx
+++ client/src/pages/Sales.jsx
@@ -1,16 +1,16 @@
 import { React, useState, useEffect } from "react";
 import { useSales } from "../context/SaleContext";
 import { useUsers } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 function Sales() {
-  const ventas = [
-    { id: 1, precio: 100 },
-    { id: 2, precio: 150 },
-    { id: 3, precio: 200 },
-  ];
   const { getSales } = useSales();
   const { getUserById } = useUsers();
   const [sales, setSales] = useState([]);
+  const navigate = useNavigate();
+  const now = new Date();
+  const currentMonth = now.getMonth();
+  const currentYear = now.getFullYear();
 
   useEffect(() => {
     (async () => {
@@ -19,11 +19,25 @@ function Sales() {
     })();
   }, []);
 
-  const totalIncome = ventas.reduce((acc, venta) => acc + venta.precio, 0);
+  const monthSales = sales.filter((sale) => {
+    const date = new Date(sale.createdAt);
+    return (
+      date.getMonth() === currentMonth && date.getFullYear() === currentYear
+    );
+  });
+
+  const monthTotal = monthSales.reduce((acc, sale) => acc + sale.price, 0);
 
   const orderedSales = [...sales].sort(
     (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
   );
+
+  const getUser = async (id) => {
+    const user = await getUserById(id);
+    console.log(user.username);
+    navigate(`/admin/${user.username}`);
+  };
+
   return (
     <>
       <h1 className="text-sm uppercase font-semibold mb-6 text-orange-50">
@@ -31,57 +45,48 @@ function Sales() {
       </h1>
       <div className="flex justify-between">
         <div className="bg-neutral-700 px-8 py-12 w-[30%] rounded-lg">
-          <span className="text-5xl font-semibold">100</span>
+          <span className="text-5xl font-semibold">{monthSales.length}</span>
           <br />
           <span>Ventas mensuales</span>
         </div>
         <div className="bg-neutral-700 px-8 py-12 w-[30%] rounded-lg">
-          <span className="text-5xl font-semibold">${totalIncome}</span>
+          <span className="text-5xl font-semibold">${monthTotal}</span>
           <br />
           Ingresos mensuales
         </div>
         <div className="bg-neutral-700 px-8 py-12 w-[30%] rounded-lg">
-          <span className="text-5xl font-semibold">100</span>
+          <span className="text-5xl font-semibold">{sales.length}</span>
           <br />
           Ventas totales
         </div>
       </div>
-      <h2 className="text-sm uppercase font-semibold my-6 text-orange-50">
+      <h2 className="text-sm uppercase font-semibold mt-6 text-orange-50">
         Ventas recientes
       </h2>
       <div className="flex flex-col gap-4">
-        {/* {orderedSales.map((sale) => (
-          <div className="bg-neutral-700 flex gap-4">
-            <h1>Plan: {sale.title}</h1>
-            <p>ID: {sale._id}</p>
-            <p>Precio: {sale.price}</p>
-            <p>Estado: {sale.status}</p>
-            <p>Id comprador: {sale.buyer}</p>
-            <p>Fecha: {new Date(sale.createdAt).toLocaleString()}</p>
-          </div>
-        ))} */}
-        <table className="w-full text-left border-collapse mt-6">
-          <thead className="bg-neutral-800 text-orange-100">
+        <table className="w-full text-left mt-6 border border-neutral-700">
+          <thead className="bg-neutral-700 text-orange-50">
             <tr>
               <th className="p-3">ID</th>
               <th className="p-3">Plan</th>
-              <th className="p-3">Comprador</th>
+              <th className="p-3">ID Comprador</th>
               <th className="p-3">Precio</th>
-              <th className="p-3">Estado</th>
               <th className="p-3">Fecha</th>
             </tr>
           </thead>
-          <tbody className="text-zinc-300">
-            {orderedSales.map(async (sale) => {
-              const user = await getUserById(sale.buyer);
-              console.log(user);
+          <tbody className="text-orange-50">
+            {orderedSales.map((sale) => {
               return (
                 <tr className="hover:bg-neutral-700">
-                  <td className="p-3">{sale._id}</td>
+                  <td className="p-3 text-sm">{sale._id}</td>
                   <td className="p-3">{sale.title}</td>
-                  <td className="p-3">{user.username}</td>
+                  <td
+                    className="p-3 cursor-pointer hover:underline text-sm"
+                    onClick={() => getUser(sale.buyer)}
+                  >
+                    {sale.buyer}
+                  </td>
                   <td className="p-3">${sale.price}</td>
-                  <td className="p-3 capitalize">{sale.status}</td>
                   <td className="p-3">
                     {new Date(sale.createdAt).toLocaleString("es-AR", {
                       dateStyle: "short",

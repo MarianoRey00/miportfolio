c2cfb54ee4bcf942d2f9f0f66edba49f9a42b787
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-17 20:13:32 -0300

    plans routes

--- server/src/routes/plan.routes.js
+++ server/src/routes/plan.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { getPlans, createPlan } from "../controllers/plan.controller.js";
 
 const router = Router();
 
-router.get();
+router.get("/plans", getPlans);
+router.post("/plans/create", createPlan);
 
 export default router;

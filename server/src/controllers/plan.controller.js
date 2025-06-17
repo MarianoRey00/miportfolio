022436a9088ae28834d67dce3dc7e06191c866b5
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-17 20:10:35 -0300

    createPan

--- server/src/controllers/plan.controller.js
+++ server/src/controllers/plan.controller.js
@@ -4,3 +4,15 @@ export const getPlans = async (req, res) => {
   const plans = await Plan.Find();
   res.status(200).json(plans);
 };
+
+export const createPlan = async (req, res) => {
+  const { title, description, price, duration, features } = req.body;
+  const newPlan = await new Plan({
+    title: title,
+    description: description,
+    price: price,
+    duration: duration,
+    features: features,
+  });
+  res.json(await newPlan.save());
+};

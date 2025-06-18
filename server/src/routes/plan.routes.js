b4bbb09ddc9427c2b5f90e9f83fe256b67898b7a
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-18 19:47:08 -0300

    validateEditPlan

--- server/src/routes/plan.routes.js
+++ server/src/routes/plan.routes.js
@@ -5,12 +5,15 @@ import {
   deletePlan,
   editPlan,
 } from "../controllers/plan.controller.js";
-import { validateCreatePlan } from "../middlewares/plan.middleware.js";
+import {
+  validateCreatePlan,
+  validateEditPlan,
+} from "../middlewares/plan.middleware.js";
 const router = Router();
 
 router.get("/plans", getPlans);
 router.post("/plans/create", validateCreatePlan, createPlan);
 router.delete("/plans/:id", deletePlan);
-router.put("/plans/:id", editPlan);
+router.put("/plans/:id", validateEditPlan, editPlan);
 
 export default router;

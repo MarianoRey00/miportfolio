9c9452415ec806eb8edbdc75f6ede4090431586b
Mariano Rey <reygonzalezmariano@gmail.com>
2025-06-18 16:39:32 -0300

    plan middleware

---
+++ server/src/middlewares/plan.middleware.js
@@ -0,0 +1,44 @@
+export const validateCreatePlan = async (req, res) => {
+  const { title, description, duration, price, features } = req.body;
+  const errors = [];
+
+  if (!title) {
+    errors.push({
+      field: "title",
+      message: "El titulo no puede estar vacio.",
+    });
+  }
+  if (!description) {
+    errors.push({
+      field: "description",
+      message: "La descripción no puede estar vacia.",
+    });
+  }
+  if (!duration) {
+    errors.push({
+      field: "duration",
+      message: "La duración no puede estar vacia.",
+    });
+  }
+  if (!price) {
+    errors.push({
+      field: "price",
+      message: "El precio no puede estar vacio",
+    });
+  }
+  if (!features) {
+    errors.push({
+      field: "features",
+      message: "Las caracteristicas no pueden estar vacias.",
+    });
+  }
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+  //   req.title = title;
+  //   req.description = description;
+  //   req.price = price;
+  //   req.duration = duration;
+  //   req.features = features;
+  next();
+};
--- server/src/routes/plan.routes.js
+++ server/src/routes/plan.routes.js
@@ -5,11 +5,11 @@ import {
   deletePlan,
   editPlan,
 } from "../controllers/plan.controller.js";
-
+import { validateCreatePlan } from "../middlewares/plan.middleware.js";
 const router = Router();
 
 router.get("/plans", getPlans);
-router.post("/plans/create", createPlan);
+router.post("/plans/create", validateCreatePlan, createPlan);
 router.delete("/plans/:id", deletePlan);
 router.put("/plans/:id", editPlan);
 
